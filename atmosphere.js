e57ddbeb463ea7620d4cb186ec64cbbce1874a05
haushinka2dx <hirano.kei1@gmail.com>
2013-04-26 08:11:59 +0900

    removed load statement for unused file.

---
+++ atmosphere.js
@@ -0,0 +1,39 @@
+load('vertx.js');
+load('atmos_debug.js');
+load('constants.js');
+load('persistor.js');
+
+var Atmosphere = function() {};
+Atmosphere.prototype = {
+	logger: vertx.logger,
+	log: function(msg) {
+		plog(Atmosphere.prototype.logger, msg);
+	},
+	dumpRequest: function(req) {
+		dump_request(Atmosphere.prototype.logger, req);
+	},
+
+	constants: getConstants(),
+	persistor: getPersistor(),
+
+	createHttpServer: function(patternHandlerMapGET, patternHandlerMapPOST, notFoundHandler) {
+		var server = vertx.createHttpServer();
+		var rm = new vertx.RouteMatcher();
+		dispatchRequestHandlers(rm, 'GET', patternHandlerMapGET, Atmosphere.prototype.logger);
+		dispatchRequestHandlers(rm, 'POST', patternHandlerMapPOST, Atmosphere.prototype.logger);
+		if (typeof(notFoundHandler) == 'undefined' || notFoundHandler == null) {
+			notFoundHandler = function(req) {
+				Atmosphere.prototype.log('noMatch');
+				dumpRequest(req);
+				req.response.sendFile('json/common/404.json');
+			};
+		}
+
+		rm.noMatch(notFoundHandler);
+		server.requestHandler(rm);1
+
+		return server;
+	},
+}
+
+var atmos = new Atmosphere();
