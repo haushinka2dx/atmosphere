db0b2ede83695e852af38af12d0991d3b7b6747c
motegi <y.motegi.6.6@gmail.co.jp>
2013-05-01 14:21:12 +0900

    起動時のエラー解消

--- atmos_debug.js
+++ atmos_debug.js
@@ -1,27 +1,9 @@
+load('time_format.js');
+
 function plog(logger, msg) {
 	var message = '[' + getCurrentTime() + '] ' + msg;
 	logger.info(message);
 	//logger.debug(message);
-}
-
-//現在時刻取得（yyyy/mm/dd hh:mm:ss）
-function getCurrentTime() {
-	var now = new Date();
-	var res = "" + now.getFullYear() + "/" + padZero(now.getMonth() + 1) + 
-		"/" + padZero(now.getDate()) + " " + padZero(now.getHours()) + ":" + 
-		padZero(now.getMinutes()) + ":" + padZero(now.getSeconds());
-	return res;
-}
-
-//先頭ゼロ付加
-function padZero(num) {
-	var result;
-	if (num < 10) {
-		result = "0" + num;
-	} else {
-		result = "" + num;
-	}
-	return result;
 }
 
 function dump_request(logger, req, enableHeaders, enableParameters, enableBody) {
---
+++ time_format.js
@@ -0,0 +1,20 @@
+//現在時刻取得（yyyy/mm/dd hh:mm:ss）
+function getCurrentTime() {
+	var now = new Date();
+	var res = "" + now.getFullYear() + "/" + padZero(now.getMonth() + 1) + 
+		"/" + padZero(now.getDate()) + " " + padZero(now.getHours()) + ":" + 
+		padZero(now.getMinutes()) + ":" + padZero(now.getSeconds());
+	return res;
+}
+
+//先頭ゼロ付加
+function padZero(num) {
+	var result;
+	if (num < 10) {
+		result = "0" + num;
+	} else {
+		result = "" + num;
+	}
+	return result;
+}
+
