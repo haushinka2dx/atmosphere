5d933e94c9dde7f0f4a40593a9aa64138fe207c0
haushinka2dx <hirano.kei1@gmail.com>
2013-09-18 15:00:07 +0900

    delayed to notify server changes to others(except for person himself)

--- main/event/listener_info.js
+++ main/event/listener_info.js
@@ -1,16 +1,27 @@
 var vertx = require('vertx');
 
+load('main/core/constants.js');
+
 function ListenerInfo(sockJSSocket, userId, eventBusAddress) {
 	this.sock = sockJSSocket;
 	this.userId = userId;
 	this.id = sockJSSocket.writeHandlerID;
 	this.eventBusAddress = eventBusAddress;
 	this.dataHandler = undefined;
+	this.delayMilliseconds = getConstants().publishDelaySeconds * 1000;
 }
 ListenerInfo.prototype.constructor = ListenerInfo;
 
 ListenerInfo.prototype.notify = function(msg) {
+	var that = this;
 	var msgJSON = JSON.parse(msg);
 	msgJSON['from_myself'] = msgJSON['from'] === this.userId;
-	this.sock.write(new vertx.Buffer(JSON.stringify(msgJSON)));
+	if (msgJSON['from_myself']) {
+		that.sock.write(new vertx.Buffer(JSON.stringify(msgJSON)));
+	}
+	else {
+		vertx.setTimer(this.delayMilliseconds, function(timerId) {
+			that.sock.write(new vertx.Buffer(JSON.stringify(msgJSON)));
+		});
+	}
 }
