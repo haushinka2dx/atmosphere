b4b6186cf39eb40504ea4126714ddf4227c8c020
m-kanno <akapanna@gmail.com>
2013-10-12 20:33:12 +0900

    test and refactor of time_format.js

--- main/util/time_format.js
+++ main/util/time_format.js
@@ -1,18 +1,30 @@
-//現在時刻取得（yyyy/mm/dd hh:mm:ss）
-function getCurrentTime() {
-	var now = new Date();
-	var res = "" + now.getFullYear() + "/" + padZero(now.getMonth() + 1) + "/" + padZero(now.getDate()) + " " + padZero(now.getHours()) + ":" + padZero(now.getMinutes()) + ":"
-			+ padZero(now.getSeconds());
-	return res;
-}
-
-// 先頭ゼロ付加
-function padZero(num) {
-	var result;
-	if (num < 10) {
-		result = "0" + num;
-	} else {
-		result = "" + num;
+var atmosTimeFormat = (function() {
+	function AtmosTimeFormat() {
 	}
-	return result;
-}
+
+	// return "yyyy/mm/dd hh:mm:ss"
+	AtmosTimeFormat.prototype.getFormatedTime = function(date) {
+		if (Object.prototype.toString.call(date) === "[object Date]") {
+			var year = date.getFullYear();
+			var month = padZero(date.getMonth() + 1);
+			var day = padZero(date.getDate());
+			var hour = padZero(date.getHours());
+			var minute = padZero(date.getMinutes());
+			var second = padZero(date.getSeconds());
+			return year + "/" + month + "/" + day + " " + hour + ":" + minute + ":" + second;
+		}
+		return '';
+	}
+
+	//現在時刻取得(yyyy/mm/dd hh:mm:ss)
+	AtmosTimeFormat.prototype.getCurrentTime = function() {
+		return this.getFormatedTime(new Date());
+	}
+
+	// 先頭ゼロ付加
+	function padZero(num) {
+		return ("0" + num).slice(-2);
+	}
+
+	return new AtmosTimeFormat();
+}());
--- spec/util/time_format_spec.js
+++ spec/util/time_format_spec.js
@@ -1,11 +1,31 @@
-describe('padZero', function(){
-	it('10未満ならば先頭に0を追加する', function(){
-		expect(padZero(0)).toEqual('00');
-		expect(padZero(9)).toEqual('09');
+describe('atmosTimeFormat', function(){
+	var target;
+
+	beforeEach(function() {
+		target = atmosTimeFormat;
 	});
 
-	it('10以上ならば何も追加しない', function(){
-		expect(padZero(10)).toEqual('10');
-		expect(padZero(100)).toEqual('100');
+	describe('getFormatedTime', function(){
+		it('渡された日時を<yyyy/mm/dd hh:mm:ss>形式の文字列にする', function(){
+			expect(target.getFormatedTime(new Date(2013, 11, 24, 23, 59, 59))).toEqual('2013/12/24 23:59:59');
+			expect(target.getFormatedTime(new Date(2013,  1, 24, 23, 59, 59))).toEqual('2013/02/24 23:59:59');
+			expect(target.getFormatedTime(new Date(2013, 11,  4, 23, 59, 59))).toEqual('2013/12/04 23:59:59');
+			expect(target.getFormatedTime(new Date(2013, 11, 24,  3, 59, 59))).toEqual('2013/12/24 03:59:59');
+			expect(target.getFormatedTime(new Date(2013, 11, 24, 23,  9, 59))).toEqual('2013/12/24 23:09:59');
+			expect(target.getFormatedTime(new Date(2013, 11, 24, 23, 59,  9))).toEqual('2013/12/24 23:59:09');
+		});
+
+		it('日付以外が渡された場合は空文字を返す', function(){
+			expect(target.getFormatedTime(null)).toEqual('');
+			expect(target.getFormatedTime(undefined)).toEqual('');
+			expect(target.getFormatedTime('')).toEqual('');
+			expect(target.getFormatedTime({})).toEqual('');
+		});
+	});
+
+	describe('getCurrentTime', function(){
+		it('現在時刻をgetFormatedTime()形式の文字列にする', function(){
+			expect(target.getCurrentTime()).toEqual(target.getFormatedTime(new Date()));
+		});
 	});
 });
